9c56480fbdffd97db3f97949678ffd1eef777e9f
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2023-01-09 18:47:35 -0500

    install dependency

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:3.8-slim-buster
 COPY . /app
 WORKDIR /app
+RUN pip install --target=/app requests
+RUN pip install --target=/app json
 CMD python3 main.py
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     my_output = f"Hello {my_input}"
 
     print(f"::set-output name=myOutput::{my_output}")
-
+    #print(myOutput=my_output" >> $GITHUB_OUTPUT)
 
 if __name__ == "__main__":
     main()
