9049956d770828965c5bf0bf2ca8d89c3dbbb39d
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2023-01-09 18:07:27 -0500

    access input

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+import os
+
+def main():
+    my_input = os.environ["IAP_TOKEN"]
+
+    my_output = f"Hello {my_input}"
+
+    print(f"::set-output name=myOutput::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
