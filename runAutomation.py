665342a4ef187facb2c33e3381a0d88f7efe3378
Ritin Bhardwaj <120592933+ritin-bhardwaj@users.noreply.github.com>
2023-04-13 13:42:16 -0400

    Update runAutomation.py

--- runAutomation.py
+++ runAutomation.py
@@ -4,28 +4,33 @@ import requests
 from actions_toolkit import core
 
 def main():
-  IAP_INSTANCE = core.get_input("IAP_INSTANCE")
-  IAP_TOKEN = core.get_input("IAP_TOKEN")
-  API_ENDPOINT = core.get_input("API_ENDPOINT")
-  API_ENDPOINT_BODY = core.get_input("API_ENDPOINT_BODY")
-  TIMEOUT = core.get_input("TIMEOUT")
-  NO_OF_ATTEMPTS = core.get_input("NO_OF_ATTEMPTS")
-  JOB_STATUS = core.get_input("JOB_STATUS")
-  print(TIMEOUT,NO_OF_ATTEMPTS,JOB_STATUS)
+  iap_instance = core.get_input("IAP_INSTANCE")
+  iap_token = core.get_input("IAP_TOKEN")
+  api_endpoint = core.get_input("API_ENDPOINT")
+  api_endpoint_body = core.get_input("API_ENDPOINT_BODY")
+  timeout = int(core.get_input("TIMEOUT"))
+  no_of_attempts = int(core.get_input("NO_OF_ATTEMPTS"))
+  job_status = core.get_input("JOB_STATUS")
   count = 0
+
+  if(iap_instance.endswith('/')):
+    iap_instance=iap_instance[:-1]
+  
+  if(api_endpoint.startswith('/')):
+    api_endpoint=api_endpoint[1:]
 
   try:
     #check the status of the job and return the output (IAP release <= 2021.1)
-    def jobStatus221(job_id,IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT):
-      response=requests.get(IAP_INSTANCE+'/operations-manager/jobs/'+job_id+'?token='+IAP_TOKEN)
+    def job_status_221(job_id,iap_instance,iap_token,count,no_of_attempts,timeout):
+      response=requests.get(iap_instance+'/operations-manager/jobs/'+job_id+'?token='+iap_token)
       if (response.status_code!=200):
         response.raise_for_status()
       print('JOB Status:',response.json()["data"]["status"])
 
-      if (response.json()["data"]["status"] == "running" and count < NO_OF_ATTEMPTS):
-        time.sleep(TIMEOUT)
+      if (response.json()["data"]["status"] == "running" and count < no_of_attempts):
+        time.sleep(timeout)
         count=count+1
-        jobStatus221(job_id,IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT)
+        job_status_221(job_id,iap_instance,iap_token,count,no_of_attempts,timeout)
 
       elif (response.json()["data"]["status"] == "complete"):
         output=response.json()["data"]["variables"]
@@ -38,25 +43,25 @@ def main():
         core.set_failed(response.json()["data"]["error"])
 
       else:
-        core.set_failed("Job Timed out based upon user defined TIMEOUT and NO_OF_ATTEMPTS")
+        core.set_failed("Job Timed out based upon user defined timeout and no_of_attempts")
 
     #check the status of the job and return the output (IAP release > 2021.1)
-    def jobStatus211(job_id,IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT):
-      response=requests.get(IAP_INSTANCE+'/workflow_engine/job/'+job_id+'/details?token='+IAP_TOKEN)
+    def job_status_211(job_id,iap_instance,iap_token,count,no_of_attempts,timeout):
+      response=requests.get(iap_instance+'/workflow_engine/job/'+job_id+'/details?token='+iap_token)
       if (response.status_code!=200):
         response.raise_for_status()
       print('JOB Status',response.json()["status"])
 
-      if (response.json()["status"] == "running" and count < NO_OF_ATTEMPTS):
-        time.sleep(TIMEOUT)
+      if (response.json()["status"] == "running" and count < no_of_attempts):
+        time.sleep(timeout)
         count=count+1
-        jobStatus211(job_id,IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT)
+        job_status_211(job_id,iap_instance,iap_token,count,no_of_attempts,timeout)
 
       elif (response.json()["status"] == "complete"):
-        result=requests.get(IAP_INSTANCE+'/workflow_engine/job/'+job_id+'/output?token='+IAP_TOKEN)
+        result=requests.get(iap_instance+'/workflow_engine/job/'+job_id+'/output?token='+iap_token)
         if (result.status_code!=200):
           result.raise_for_status()
-        core.set_output(f"::set-output name=results::{result.json()}")
+        core.set_output("results",result.json())
 
       elif (response.json()["status"] == "canceled"):
         core.set_failed("Job Canceled")
@@ -65,27 +70,32 @@ def main():
         core.set_failed(response.json()["error"])
 
       else:
-        core.set_failed("Job Timed out based upon user defined TIMEOUT and NO_OF_ATTEMPTS")
+        core.set_failed("Job Timed out based upon user defined timeout and no_of_attempts")
 
-    def startJob(IAP_INSTANCE):
+    def start_job(iap_instance):
       #API call to get IAP release
-      release=requests.get(IAP_INSTANCE+'/health/server',params={'token':IAP_TOKEN})
+      release=requests.get(iap_instance+'/health/server',params={'token':iap_token})
       if (release.status_code!=200):
         release.raise_for_status()
-      iapRelease=release.json()["release"][0:release.json()["release"].rindex(".")]
+      iap_release=release.json()["release"][0:release.json()["release"].rindex(".")]
 
       #API call to start API Endpoint trigger
-      response=requests.post(IAP_INSTANCE+'/operations-manager/triggers/endpoint/'+API_ENDPOINT+'?token='+IAP_TOKEN,json=json.loads(API_ENDPOINT_BODY))
+      response=requests.post(iap_instance+'/operations-manager/triggers/endpoint/'+api_endpoint+'?token='+iap_token,json=json.loads(api_endpoint_body))
       if (response.status_code!=200):
         response.raise_for_status()
-      if bool(int(JOB_STATUS))==True:
-        if(float(iapRelease)<=2021.1):
-          jobStatus211(response.json()["_id"],IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT)
+      if bool(int(job_status)) is True:
+        if(float(iap_release)<=2021.1):
+          job_status_211(response.json()["_id"],iap_instance,iap_token,count,no_of_attempts,timeout)
         else:
-          jobStatus221(response.json()["data"]["_id"],IAP_INSTANCE,IAP_TOKEN,count,NO_OF_ATTEMPTS,TIMEOUT)
+          job_status_221(response.json()["data"]["_id"],iap_instance,iap_token,count,no_of_attempts,timeout)
+      else:
+        if(float(iap_release)<=2021.1):
+          core.set_output("results",response.json()["variables"])
+        else:
+          core.set_output("results",response.json()["data"]["variables"])
         
 
-    startJob(IAP_INSTANCE)
+    start_job(iap_instance)
   except requests.exceptions.RequestException as err:
     core.set_failed(err)
 
